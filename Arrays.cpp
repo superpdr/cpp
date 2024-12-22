c490becff377252b3314c98ea6783885a003fac8
Superpdr <piyushraipure2007@gmail.com>
2024-12-22 17:41:35 +0530

    Accessing the element in array

--- Arrays.cpp
+++ Arrays.cpp
@@ -2,6 +2,9 @@
     Array - collection of the same type of element 
              fixed size of the array 
     need - like a container to store more than one values 
+    array elements are stored in contiguous memory location 
+    the address of the array is base i.e the first element.
+    offsets : the index of the array 
 
 */
 
@@ -9,12 +12,14 @@
 using namespace std ;
 
 void Array_Test();
+void Array_AccessElement_Test();
 
 int main()
 {
-    Array_Test();
+    // Array_Test();
     //   int random_numbers[10];
     //   cout << 5 <= sizeof(random_numbers)/4 <<endl;
+    Array_AccessElement_Test();
     return 0 ;
 
 }
@@ -32,5 +37,21 @@ void Array_Test()
     for (auto itr : random_numbers)
     {
         cout << itr <<endl ;
+    }
+}
+
+void Array_AccessElement_Test()
+{
+    int random_numbers[5];
+    for(int i = 0 ; i<5; ++i)
+    {
+        cout << "enter the elements"<<endl;
+        cin >> random_numbers[i];
+    }
+
+    cout << " base address"<<random_numbers<<endl ;
+    for (auto &itr : random_numbers)
+    {
+        cout << " the adrress : "<< &itr<<endl;
     }
 }
