f738f02e5ec5f6fe683c565dbcf7cefe44850a83
Piyush Raipure <108759231+superpdr@users.noreply.github.com>
2024-12-27 16:13:03 +0530

    Add files via upload

--- Arrays.cpp
+++ Arrays.cpp
@@ -6,6 +6,12 @@
     the address of the array is base i.e the first element.
     offsets : the index of the array 
 
+
+    multi dimension Array :
+                datatype arrayname [size 1][size 2].....[size n ]
+                if the value are not provided then  the value would be set to zero
+
+
 */
 
 #include<iostream>
@@ -13,13 +19,15 @@ using namespace std ;
 
 void Array_Test();
 void Array_AccessElement_Test();
+void Array_2D_Test();
 
 int main()
 {
     // Array_Test();
     //   int random_numbers[10];
     //   cout << 5 <= sizeof(random_numbers)/4 <<endl;
-    Array_AccessElement_Test();
+    // Array_AccessElement_Test();
+    Array_2D_Test();
     return 0 ;
 
 }
@@ -54,4 +62,21 @@ void Array_AccessElement_Test()
     {
         cout << " the adrress : "<< &itr<<endl;
     }
+}
+
+void Array_2D_Test()
+{
+    int array2d [3][3] = {0,1,2,3,4,5,6,7,8};
+    //  cout << array2d[0][1];
+    int collection [2][2];
+
+    for(int i = 0 ; i < 2 ; ++i)
+    {
+        for(int j = 0 ; j < 2 ; ++j)
+        {
+            cout << " enter "<< i <<" "<<j << " th element :"<<endl;
+            cin >> collection[i][j];
+        }
+    }
+
 }
